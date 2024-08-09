180fcb860ab3e3014f07bdea67e8399dca7fcaab
monasubedi <sbd.mona@gmail.com>
2024-08-08 21:41:21 -0400

    add stack question 2

--- stack.js
+++ stack.js
@@ -33,21 +33,57 @@
 //Input: " hello world "    -> Output: "world hello"
 //Input: "a good  example"  -> Output: "example good a"
 
-function reverseWords(str) {
-    const splits = str.split(" ");
+// function reverseWords(str) {
+//     const splits = str.split(" ");
+//     let stack = [];
+//     for (let i of splits) {
+//         stack.push(i);
+//     }
+//     let finalStr = "";
+//     while (stack.length) {
+//         let current = stack.pop();
+//         if (current) {
+//             finalStr += " " + current;
+//         }
+//     }
+//     return finalStr.trim();
+// }
+
+// console.log(reverseWords("the sky is blue"));
+// console.log(reverseWords("a good  example"));
+
+
+//Ques 2: Given a string s containing just the characters '(', ')', '{','}','[',']'
+//         determine if the input string is valid.
+//Open brackets must be closed by the same type of brackets.
+//Open brackets must be closed in the correct order.
+//Every close bracket has a corresponding open bracket of the same type.
+
+//Input : "()"     -> Output: true
+//Input : "([]{})" -> Output: true
+//Input : "(]"     -> Output: false
+
+function isValid(s) {
     let stack = [];
-    for (let i of splits) {
-        stack.push(i);
-    }
-    let finalStr = "";
-    while (stack.length) {
-        let current = stack.pop();
-        if (current) {
-            finalStr += " " + current;
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+
+        if (char === "(" || char === "{" || char === "[") {
+            stack.push(char);
+        }
+        else if (char === ")" || char === "}" || char === "]") {
+            if (isEmpty(stack)) {
+                return false;
+            }
+            const top = stack.pop();
+            if ((char === ")" && top !== "(") || (char === "]" && top !== "[") || (char === "}")) {
+                return false;
+            }
         }
     }
-    return finalStr.trim();
+    return isEmpty(stack);
 }
 
-console.log(reverseWords("the sky is blue"));
-console.log(reverseWords("a good  example"));
+function isEmpty(stack) {
+    return stack.length === 0;
+}
